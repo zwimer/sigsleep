5c9c5da8c0d45dcf2634832bbad2761798e59996
zwimer <github@zwimer.com>
2025-06-12 23:26:53 -0400

    Handle suffix arguments and be more robust

--- sigsleep/sigsleep.py
+++ sigsleep/sigsleep.py
@@ -1,3 +1,4 @@
+from math import isnan
 import argparse
 import signal
 import time
@@ -6,27 +7,50 @@ import sys
 from ._version import __version__
 
 
-def _handler(total: float, start: int, *_) -> None:
+def _handler(total: int | float, start: int, *_) -> None:
     """
     Bind total and start to make into a signal handler that prints elapsed time
     """
-    remain: int = round(total - (time.time_ns() - start) / 1e9)
-    t: str = str(int(total) if int(total) == total else total)
+    if total != float("inf"):
+        remain: int | str = round(max(0, round(total - (time.time_ns() - start) / 1e9)))
+        t: int | float | str = int(total) if int(total) == total else total
+    else:
+        t, remain = ("infinity", "infinity")
     print(f"sleep: about {remain} seconds(s) left out of the original {t}")
 
 
-def sigsleep(seconds: float, sig: signal.Signals) -> int:
+def sigsleep(seconds: int | float, sig: signal.Signals) -> int:
     """
     Sleep, print elapsed time on intercepting signal
     :return Exit code
     """
+    if seconds < 0:  # Just in case
+        raise ValueError("sleep length must be non-negative")
+    if isnan(seconds):  # Just in case
+        raise ValueError("sleep length may not be NaN")
     start: int = time.time_ns()
     signal.signal(sig, lambda *_: _handler(seconds, start))
     try:
-        time.sleep(seconds)
+        # Split sleep into multiple calls for large numbers, also
+        # OS Schedulers can be wonky and have limits under the theoretical 2^64
+        max_sleep = 1  # 1E9
+        while seconds > 0:
+            time.sleep(sec := min(seconds, max_sleep))
+            seconds -= sec
     except KeyboardInterrupt:
         return 130
     return 0
+
+
+def length(num: str) -> int | float:  # Name it length so that argparse error messages are cleaner
+    """
+    Convert the NUMBER[SUFFIX] num into an int or float
+    """
+    if not (n := num.lower()):
+        raise ValueError("Empty string is not a valid number")
+    if (scale := {"m": 60, "h": 3600, "d": 3600 * 24}.get(n[-1], 1)) != 1 or n[-1] == "s":
+        n = n[:-1]
+    return (int(n) if n.isdigit() else float(n)) * scale
 
 
 def cli() -> None:
@@ -36,7 +60,13 @@ def cli() -> None:
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action="version", version=f"{parser.prog} {__version__}")
-    parser.add_argument("seconds", type=float, help="The number of seconds to sleep")
+    parser.add_argument(
+        "seconds",
+        metavar="NUMBER[SUFFIX]",
+        type=length,
+        nargs="+",
+        help="Pause for NUMBER seconds, where NUMBER is an integer or floating-point (inf is allowed). SUFFIX may be 's', 'm', 'h', or 'd', for seconds, minuts, hours, days. With multiple arguments, pause for the sum of their values.",
+    )
     to_signal = lambda d: signal.Signals(int(d))
     to_signal.__name__ = "signal"  # Cleaner output
     parser.add_argument(
@@ -47,4 +77,4 @@ def cli() -> None:
         default=signal.SIGINFO if hasattr(signal, "SIGINFO") else signal.SIGUSR1,
     )
     ns = parser.parse_args()
-    sys.exit(sigsleep(ns.seconds, ns.signal))
+    sys.exit(sigsleep(sum(ns.seconds), ns.signal))
